4c184f7d6dd0fe35104dcf97b9a28eb7e80113ba
sapnakummar24 <163388570+sapnakummar24@users.noreply.github.com>
2024-03-14 12:34:40 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,22 @@
+//1 Write a program to take three variables to store your birth date, birth month, and birth year, respectively, and then print them one by one in a specified order.
+// const input = require('readline-sync')
+// let a = input.question("enter the birth date");
+// let b = input.question("enter the birth month");
+// let c = input.question("enter the birth year");
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+///2 Write a program to take input from the user, and then print it to the output.
+// const input = require ('readline-sync')
+// let n = input.questionInt("enter the number");
+// console.log(n);
+
+
+//3 Write a program to take two values from the user and swap them. 
+// const input = require('readline-sync')
+// let a = input.questionInt('enter number a');
+// let b = input.questionInt('enter number b');
+// console.log(b);
+// console.log(a);
